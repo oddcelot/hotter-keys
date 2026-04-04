e8811fd9e8603ead69cf68b83be08da0bbf5e43c
Stefan Kopco <hey@oddcelot.dev>
2026-04-04 14:44:28 +0200

    feat: Add demo project for hotter-keys
    Includes a basic SolidJS application setup with Vite and Hotter Keys
    devtools.
    This allows for easier local development and testing of the core
    library.

--- examples/demo/vite.config.ts
+++ examples/demo/vite.config.ts
@@ -4,5 +4,5 @@ import solidPlugin from "vite-plugin-solid";
 import { hotterKeysViteDevtools } from "@hotter-keys/devtools/vite";
 
 export default defineConfig({
-  plugins: [DevTools({ auth: false }), solidPlugin(), hotterKeysViteDevtools()],
+  plugins: [DevTools({ clientAuth: false }), solidPlugin(), hotterKeysViteDevtools()],
 });
